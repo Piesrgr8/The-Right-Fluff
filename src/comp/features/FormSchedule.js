2ddc3bde4e504e959eb739a0ae19e2a419cb4e2a
Jared Bowman <jaredbruiser@hotmail.com>
2024-05-17 22:38:10 -0400

    Update form to eliminate the possibility of 2 emails being sent

--- src/comp/features/FormSchedule.js
+++ src/comp/features/FormSchedule.js
@@ -5,15 +5,15 @@ import ReCAPTCHA from 'react-google-recaptcha'
 import '../../assets/css/form.css'
 
 export default function FormSchedule(props) {
+    const service_id = process.env.REACT_APP_SERVICE_ID
+    const template_id = process.env.REACT_APP_TEMPLATE
+    const public_key = process.env.REACT_APP_PUBLIC_KEY
 
-    let service_id = process.env.REACT_APP_SERVICE_ID;
-    let template_id = process.env.REACT_APP_TEMPLATE;
-    let public_key = process.env.REACT_APP_PUBLIC_KEY;
-
-    // const list = props.flavors;
-    const list2 = props.events;
+    const list2 = props.events
 
     const [sent, setSent] = useState(false)
+    const [btnHide, setBtnHide] = useState(false)
+    const [captchaVerified, setCaptchaVerified] = useState(false)
 
     const [send, setSend] = useState({
         first_name: '',
@@ -29,156 +29,209 @@ export default function FormSchedule(props) {
         message: '',
     })
 
-    const form = useRef();
+    const form = useRef()
 
     const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.sendForm(service_id, template_id, form.current, public_key)
-          .then((result) => {
-              console.log(result.text);
-              setSent(true);
-          }, (error) => {
-              console.log(error.text);
-              setSent(false);
-          });
-      };
-
-    const changeHandler = (e) => {
-        setSend({ [e.target.name]: e.target.value })
+        e.preventDefault()
+        emailjs
+            .sendForm(service_id, template_id, form.current, public_key)
+            .then(
+                (result) => {
+                    console.log(result.text)
+                    setSent(true)
+                },
+                (error) => {
+                    console.log(error.text)
+                    setSent(false)
+                    setBtnHide(false)
+                }
+            )
     }
 
-    // const changeArrayHandler = (e) => {
-    //     var newList = checked
-    //     if (e.target.checked) {
-    //         newList.push(e.target.value)
-    //     } else {
-    //         newList.splice(checked.indexOf(e.target.value), 1)
-    //     }
+    const changeHandler = (e) => {
+        setSend((prevSend) => ({
+            ...prevSend,
+            [e.target.name]: e.target.value,
+        }))
+    }
 
-    //     newList.sort()
-    //     setChecked(newList)
-    // }
+    const handleCaptchaChange = (value) => {
+        setCaptchaVerified(!!value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const requiredFields = [
+            'first_name',
+            'last_name',
+            'email',
+            'date',
+            'fromTime',
+            'toTime',
+            'venue',
+            'eventTyp',
+            'address',
+        ]
+        const allFieldsFilled = requiredFields.every((field) => send[field])
+
+        if (allFieldsFilled && captchaVerified) {
+            setBtnHide(true)
+            sendEmail(e)
+        } else if (!captchaVerified) {
+            alert('Please complete the reCAPTCHA.')
+        } else {
+            alert('Please fill in all required fields.')
+        }
+    }
 
     return (
         <div className="formdata">
-            {sent ? <p id='sent'>Sent! We will review this data and get to you shortly!</p> :
-            <form ref={form} onSubmit={sendEmail}>
-                <p>Contact Information</p>
-                <section>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        name="first_name"
-                        value={send.first_name}
-                        onChange={changeHandler}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        name="last_name"
-                        value={send.last_name}
-                        onChange={changeHandler}
-                    />
-                </section>
-                <section>
-                    <input
-                        type="text"
-                        placeholder="E-Mail"
-                        name="email"
-                        value={send.email}
-                        onChange={changeHandler}
-                        required
-                    />
-                    <input 
-                        type="tel"
-                        placeholder="Phone #"
-                        name="phone"
-                        value={send.phone}
-                        onChange={changeHandler}
-                    />
-                </section>
-                <p>Date & Time</p>
-                <section>
-                    <input
-                        type="date"
-                        id='date'
-                        placeholder="01/01/2023"
-                        name="date"
-                        value={send.date}
-                        onChange={changeHandler}
-                    />
-                </section>
-                <section>
-                    <p>From:</p>
-                    <input
-                        type="time"
-                        id='time'
-                        placeholder="12:00 PM"
-                        name="time"
-                        value={send.fromTime}
-                        onChange={changeHandler}
-                    />
-                    <p>To:</p>
-                    <input
-                        type="time"
-                        id='time'
-                        placeholder="12:00 PM"
-                        name="time"
-                        value={send.toTime}
-                        onChange={changeHandler}
-                    />
-                </section>
-                <p>Venue & Party</p>
-                <section style={{ width: 'fit-content' }}>
-                    <input
-                        type="radio"
-                        id="in"
-                        name="venue"
-                        value="Indoor"
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="in">Indoor</label>
+            {sent ? (
+                <p id="sent">
+                    Sent! We will review this data and get to you shortly!
+                </p>
+            ) : (
+                <form ref={form} onSubmit={handleSubmit}>
+                    <p>Contact Information</p>
+                    <section>
+                        <input
+                            type="text"
+                            placeholder="First Name"
+                            name="first_name"
+                            value={send.first_name}
+                            onChange={changeHandler}
+                            required
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
+                            name="last_name"
+                            value={send.last_name}
+                            onChange={changeHandler}
+                            required
+                        />
+                    </section>
+                    <section>
+                        <input
+                            type="text"
+                            placeholder="E-Mail"
+                            name="email"
+                            value={send.email}
+                            onChange={changeHandler}
+                            required
+                        />
+                        <input
+                            type="tel"
+                            placeholder="Phone #"
+                            name="phone"
+                            value={send.phone}
+                            onChange={changeHandler}
+                        />
+                    </section>
+                    <p>Date & Time</p>
+                    <section>
+                        <input
+                            type="date"
+                            id="date"
+                            placeholder="01/01/2023"
+                            name="date"
+                            value={send.date}
+                            onChange={changeHandler}
+                            required
+                        />
+                    </section>
+                    <section>
+                        <p>From:</p>
+                        <input
+                            type="time"
+                            id="fromTime"
+                            placeholder="12:00 PM"
+                            name="fromTime"
+                            value={send.fromTime}
+                            onChange={changeHandler}
+                            required
+                        />
+                        <p>To:</p>
+                        <input
+                            type="time"
+                            id="toTime"
+                            placeholder="12:00 PM"
+                            name="toTime"
+                            value={send.toTime}
+                            onChange={changeHandler}
+                            required
+                        />
+                    </section>
+                    <p>Venue & Party</p>
+                    <section style={{ width: 'fit-content' }}>
+                        <input
+                            type="radio"
+                            id="in"
+                            name="venue"
+                            value="Indoor"
+                            onChange={changeHandler}
+                            required
+                        />
+                        <label htmlFor="in">Indoor</label>
 
-                    <input
-                        type="radio"
-                        id="out"
-                        name="venue"
-                        value="Outdoor"
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="out">Outdoor</label>
-                </section>
-                <section>
-                    <select name='eventTyp' id='event'>
-                        <option value="not">Choose an option...</option>
-                        {list2.map((m) => 
-                        <option key={m.id} value={m.shortname}>{m.name}</option>
-                        )}
-                    </select>
-                </section>
-                <section>
-                    <input
-                        type="text"
-                        placeholder="Address"
-                        name="address"
-                        value={send.address}
-                        onChange={changeHandler}
-                    />
-                </section>
-                <p>Give us an idea on what your event is about! (Optional)</p>
-                <section>
-                    <textarea name='message' onChange={changeHandler}></textarea>
-                </section>
-                <span>
-                    <ReCAPTCHA
-                        sitekey="6LfqsdIiAAAAAMkT6RAT4D9KOQgnpmPRDi5CjAsq"
-                        size="normal"
-                    />
-                </span>
-                <button type="submit" name="done">
-                    Schedule
-                </button>
-            </form>}
+                        <input
+                            type="radio"
+                            id="out"
+                            name="venue"
+                            value="Outdoor"
+                            onChange={changeHandler}
+                            required
+                        />
+                        <label htmlFor="out">Outdoor</label>
+                    </section>
+                    <section>
+                        <select
+                            name="eventTyp"
+                            id="event"
+                            onChange={changeHandler}
+                            required
+                        >
+                            <option value="">Choose an option...</option>
+                            {list2.map((m) => (
+                                <option key={m.id} value={m.shortname}>
+                                    {m.name}
+                                </option>
+                            ))}
+                        </select>
+                    </section>
+                    <section>
+                        <input
+                            type="text"
+                            placeholder="Address"
+                            name="address"
+                            value={send.address}
+                            onChange={changeHandler}
+                            required
+                        />
+                    </section>
+                    <p>
+                        Give us an idea on what your event is about! (Optional)
+                    </p>
+                    <section>
+                        <textarea
+                            name="message"
+                            onChange={changeHandler}
+                        ></textarea>
+                    </section>
+                    <span>
+                        <ReCAPTCHA
+                            sitekey="6LfqsdIiAAAAAMkT6RAT4D9KOQgnpmPRDi5CjAsq"
+                            size="normal"
+                            onChange={handleCaptchaChange}
+                        />
+                    </span>
+                    {!btnHide && (
+                        <button type="submit" name="done">
+                            Schedule
+                        </button>
+                    )}
+                </form>
+            )}
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
 import '../assets/css/Pages.css'
 import React from 'react'
-import {flavors, partyType} from '../comp/important/Options'
+import { flavors, partyType } from '../comp/important/Options'
 import logo from '../assets/img/logo.png'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-const list1 = flavors;
-const list2 = partyType;
+const list1 = flavors
+const list2 = partyType
 
 export default function Home(props) {
-    const imgs = props.images;
+    const imgs = props.images
     return (
         <div className="home">
             <div className="home-header">
-                <img id='logopop' src={logo} alt=''/>
+                <img id="logopop" src={logo} alt="" />
                 <div className="carousel">
                     <div className="carousel-track">
                         <div className="slide">
@@ -47,35 +47,67 @@ export default function Home(props) {
                     </div>
                 </div>
             </div>
-            <div className="wrap" aria-label="Main Page"  aria-required="true">
+            <div className="wrap" aria-label="Main Page" aria-required="true">
                 {/* <InfoCard /> */}
                 <h1>Put a Fresh Spin on your Next Event</h1>
-                <p>Make your next event a little sweeter with fresh live spinning and flavorful treats for your guests.</p>
-                <ul id='events'>
+                <p>
+                    Make your next event a little sweeter with fresh live
+                    spinning and flavorful treats for your guests.
+                </p>
+                <ul id="events">
                     {list2.map((m) =>
-                        m.banner ? 
-                            <li key={m.id} style={{'backgroundImage': 'url(' + m.banner + ')'}}><p>{m.name}</p></li>
-                            :
-                            <li style={{'display': 'none'}}><p>none</p></li>
-                        
+                        m.banner ? (
+                            <li
+                                key={m.id}
+                                style={{
+                                    backgroundImage: 'url(' + m.banner + ')',
+                                }}
+                            >
+                                <p>{m.name}</p>
+                            </li>
+                        ) : (
+                            <li style={{ display: 'none' }}>
+                                <p>none</p>
+                            </li>
+                        )
                     )}
                 </ul>
-                <p>Inside or outdoors, day or night, rain or shine, we provide a fun and memorable highlight to any event. Our cotton candy is spun fresh from pure cane sugar. Certified Organic, Gluten-free, Kosher, Vegan, and free of arfticial colors.</p>
-                <hr/>
+                <p>
+                    Inside or outdoors, day or night, rain or shine, we provide
+                    a fun and memorable highlight to any event. Our cotton candy
+                    is spun fresh from pure cane sugar. Certified Organic,
+                    Gluten-free, Kosher, Vegan, and free of arfticial colors.
+                </p>
+                <hr />
                 <h1>A Flavorful Twist on a Traditional Treat</h1>
-                <p>We take those sweet memories of days at the county fair to a new level with gourmet flavors your guests will love. Our favorites include:</p>
-                <ul id='flavors'>
-                    {list1.map((m) =>
-                    <li key={m.id} style={{'backgroundColor': m.basecolor}}><p style={{'color': m.id !== 5 ? 'black' : 'white'}}>{m.name}</p></li>
-                    )}
+                <p>
+                    We take those sweet memories of days at the county fair to a
+                    new level with gourmet flavors your guests will love. Our
+                    favorites include:
+                </p>
+                <ul id="flavors">
+                    {list1.map((m) => (
+                        <li key={m.id} style={{ backgroundColor: m.basecolor }}>
+                            <p
+                                style={{
+                                    color: m.id !== 5 ? 'black' : 'white',
+                                }}
+                            >
+                                {m.name}
+                            </p>
+                        </li>
+                    ))}
                 </ul>
-                <p>Interested in a flavor that's not on this list?</p>
-                <Link to={"/contact"}><strong id='link'>Contact us!</strong></Link>
-                <hr/>
+                <hr style={{ marginTop: '30px' }} />
                 <h1>Pricing</h1>
-                <p>Book a live spinner for any size event, from backyard barbecues to all-day festivals. Our prices start at $250 for the 1st hour for up to 50 guests. Prices fluctuate with the size and duration of your event and flavor choices.</p>
-                <Link to={"/contact"}>Contact us!</Link>
-                <br/>
+                <p>
+                    Book a live spinner for any size event, from backyard
+                    barbecues to all-day festivals. Our prices start at $250 for
+                    the 1st hour for up to 50 guests. Prices fluctuate with the
+                    size and duration of your event and flavor choices.
+                </p>
+                <Link to={'/contact'}>Contact us!</Link>
+                <br />
             </div>
         </div>
     )
